8340889bc41e4268c6c4f2c97f00a3fbab910eb3
maomaoliang <maomaoliang@users.noreply.github.com>
2026-03-09 19:26:23 +0800

    remove: 移除看板娘 Live2D

--- source/js/live2d-widget.js
+++
@@ -1,7 +0,0 @@
-// [H] 看板娘 Live2D Widget
-// 使用 live2d-widget CDN 方案，轻量无需安装额外依赖
-(function() {
-  var script = document.createElement('script');
-  script.src = 'https://fastly.jsdelivr.net/gh/stevenjoezhang/live2d-widget@latest/autoload.js';
-  document.head.appendChild(script);
-})();
